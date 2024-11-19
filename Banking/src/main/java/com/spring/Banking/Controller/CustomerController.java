8604517fb5384b5f36749dca5689e38948520ec6
Dhruv-Dugar <dhruvdugar02@gmail.com>
2024-11-19 06:45:34 +0000

    customer controller with all the path

--- Banking/src/main/java/com/spring/Banking/Controller/CustomerController.java
+++ Banking/src/main/java/com/spring/Banking/Controller/CustomerController.java
@@ -1,4 +1,38 @@
 package com.spring.Banking.Controller;
 
+
+import com.spring.Banking.Entity.CustomerEntity;
+import com.spring.Banking.Service.CustomerService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/customer")
 public class CustomerController {
+
+	private final CustomerService customerService;
+
+
+	@Autowired
+	public CustomerController(CustomerService customerService){
+		this.customerService = customerService;
+	}
+
+	@GetMapping("/{id}")
+	public CustomerEntity getCustomerById(@PathVariable("id") long customerId){
+		return customerService.getCustomerById(customerId);
+	}
+
+	@PostMapping
+	public CustomerEntity createCustomer(@RequestBody CustomerEntity customer){
+		return customerService.createCustomer(customer);
+	}
+
+	@PutMapping("/{id}")
+	public CustomerEntity updateCustomer(@RequestBody CustomerEntity customer, @PathVariable long    customerId){
+		return customerService.updateCustomer(customer, customerId);
+	}
+
+	
+	// bank has no delete method due to KYC reasons and regulations
 }
