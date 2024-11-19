393d72845ace48e1a96398c2aaa6e1da42cb5245
Ritvik <ritvikrrao@gmail.com>
2024-11-19 10:32:33 +0000

    added user registration and login

---
+++ Banking/src/main/java/com/spring/Banking/Entity/Role.java
@@ -0,0 +1,6 @@
+package com.spring.Banking.Entity;
+
+public enum Role {
+    ADMIN,
+    USER
+}
---
+++ Banking/src/main/java/com/spring/Banking/Repository/UserRepository.java
@@ -0,0 +1,14 @@
+package com.spring.Banking.Repository;
+
+import com.spring.Banking.Entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Integer> {
+    // public  User findByEmailId(String emailId);
+
+    public User findByEmail(String email);
+
+    public boolean existsByEmail(String email);
+}
